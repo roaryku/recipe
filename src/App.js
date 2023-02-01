6e62d896c51adcc773aa29bd681736a4ebe9e47e
Rodika <raryku@yahoo.com>
2023-01-31 20:53:58 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+
+import { useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const MY_ID = "df6f65fe";
+  const MY_KEY = "afc456bb7ac227df48ba6ba00ca1f83e	-";
+
+  
+
+  useEffect(() => {
+    const getNewRecipe = async () => {
+    const responce = await fetch("https://api.edamam.com/api/recipes/v2?type=public&q=avocado&app_id=${MY_ID}&app_key=${MY_KEY}");
+    const data = await responce.json();
+    console.log(data)
+  }
+  getNewRecipe()
+  })
+  
+
+  return(
+    <div className='App'>
+      <p>RECIPES</p>
     </div>
-  );
-}
+  )
+  }
+
+
+
+
 
 export default App;
